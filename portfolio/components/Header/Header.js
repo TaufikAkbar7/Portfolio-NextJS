856f028e229f667a6321fb9828bac0a865a46399
TaufikAkbar7 <taufik.lol911@gmail.com>
2021-06-21 13:25:03 +0700

    buat file komponen dan install tailwind

---
+++ portfolio/components/Header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="bg-gray-500">
+            Header
+        </div>
+    )
+}
+
+export default Header
---
+++ portfolio/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header/Header.js"
+import Footer from "./Footer/Footer.js"
+import Hero from "./Hero/Hero.js"
+import Projects from "./Projects/Projects.js"
+
+export { Header, Footer, Hero, Projects }
