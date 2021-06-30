2285a663a251b47533c9cbb0527c947241d7c4b3
TaufikAkbar7 <taufik.lol911@gmail.com>
2021-06-30 09:26:23 +0700

    edit components/index

--- portfolio/components/index.js
+++ portfolio/components/index.js
@@ -1,7 +1,4 @@
-import Header from "./Header/Header.js"
-import Experience from "./Experience/Experience.js"
 import Hero from "./Hero/Hero.js"
 import Projects from "./Projects/Projects.js"
-import Technologies from "./Technologies/Technologies.js"
 
-export { Header, Experience, Technologies, Hero, Projects }
+export { Hero, Projects }
