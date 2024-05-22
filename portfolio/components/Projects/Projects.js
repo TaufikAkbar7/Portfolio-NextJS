00007145ecd3548a4a2b905ead7b32cb64b22885
TaufikAkbar7 <taufik.lol911@gmail.com>
2024-05-22 15:43:38 +0700

    update projects

--- portfolio/components/Projects/Projects.js
+++ portfolio/components/Projects/Projects.js
@@ -1,32 +1,41 @@
 import { project } from "../../content/data";
 import { Judul } from "../atoms";
-// import { useEffect } from "react";
-import Link from 'next/link';
+import { FaExternalLinkAlt, FaGithub, FaRegFolderOpen } from "react-icons/fa";
 
 const Projects = () => {
 
   return (
-    <div className="min-h-screen flex flex-col justify-start items-start overflow-x-hidden section-project mb-10 projects">
+    <div className="min-h-screen flex flex-col justify-start items-center overflow-x-hidden section-project mb-10 projects">
       <Judul color="text-white" text={project.title} marginTop="mt-14" width="w-40" height="h-2" fontSize="text-4xl" fontWeight="font-bold" />
       <p className="sm:text-lg mt-2 text-white">{project.subtitle}</p>
-      <div className="flex flex-wrap flex-row justify-start items-center mt-2">
+      <div className="grid grid-cols-1 mt-8 gap-5 lg:grid-cols-2 xl:grid-cols-3">
         {project.projects.map((item, i) => (
-          <div className="mt-5 w-full sm:w-72 md:mt-5 md:w-80 sm:mt-5 sm:ml-1 sm:mr-3 h-full" key={i}>
-            <div className="h-full rounded-lg overflow-hidden bg-gray-700">
-              <div className="p-6 flex flex-col justify-center">
-                <Judul color="text-white" marginTop="mt-2" text={item.project_name} fontSize="text-2xl" fontWeight="font-medium" />
-                <div className="mt-4 relative items-center">
-                  <p className="text-white text-sm tracking-normal font-inter">
-                    {item.project_title}
-                  </p>
-                </div>
-                <Link href={{
-                  pathname: '/detail',
-                  query: { id: item.id, }
-                }}>
-                  <span className="text-primaryColor w-24 mt-4 border-b-2 border-transparent hover:border-primaryColor cursor-pointer">Go to project</span>
-                </Link>
+          <div className="flex flex-col justify-between w-80 h-72 p-6 rounded-lg overflow-hidden bg-gray-700" key={i}>
+            <div className="flex justify-between items-center w-full">
+              <FaRegFolderOpen className="w-8 h-8 text-primaryColor" />
+              <div className="flex items-center gap-x-3">
+                {item.linkWebsite && (
+                  <a href={item.linkWebsite} target="_blank">
+                    <FaExternalLinkAlt className="w-5 h-5 text-primaryColor" />
+                  </a>
+                )}
+                <a href={item.linkGithub} target="_blank">
+                  <FaGithub className="w-5 h-5 text-primaryColor" />
+                </a>
               </div>
+            </div>
+            <div className="flex flex-col justify-center">
+              <Judul color="text-white" marginTop="mt-2" text={item.project_name} fontSize="text-2xl" fontWeight="font-medium" />
+              <div className="mt-4 relative items-center">
+                <p className="text-white text-sm tracking-normal font-inter">
+                  {item.project_title}
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-x-3 gap-y-0.5 items-center mt-3">
+              {item.stacks.map((stack, stackItem) => (
+                <p key={stack + stackItem} className="text-sm text-gray-400">{stack}</p>
+              ))}
             </div>
           </div>
         ))}
--- portfolio/content/data.js
+++ portfolio/content/data.js
@@ -1,23 +1,11 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-// import { FiDownload } from "react-icons/fi";
 import { FiInstagram } from "react-icons/fi";
 import { SiGmail } from "react-icons/si";
 import { BiLinkExternal } from "react-icons/bi";
-import { 
-    codingNiceHome,
-    shopmerceHome,
-    shopmerceImage1,
-    shopmerceImage2,
-    shopmerceImage3,
-    shopmerceImage4,
-    shopmerceImage5,
-    jobApps,
-    webfood
-} from '../public/assets/index';
 
 const hero = {
     title: "Hi. I'm Taufik Akbar",
-    desc: "An undergraduate student majoring in Informatics Engineering at University Muhammadiyah Prof. Dr. Hamka. I love coding and i aspire to become a Front End Developer"
+    desc: "I'm a Frontend Developer based in Jakarta, Indonesia. Specializing in building exceptional websites, applications, and everything in between."
 }
 
 
@@ -26,126 +14,64 @@ const project = {
     subtitle: "Some projects that i'm proud of",
     projects: [
         {
-            id: 1,
-            project_name: "Coding Nicely",
-            project_title: "Coding Nicely is website landing page for startup based on course digital",
-            project_desc: "In July 2021 I was created a website landing page by theme course. In this project i was practice for slicing design.",
-            features: ["Server-side Rendering", "Image Optimization", "Caraousel", "Responsive Design"],
-            image: [
-                codingNiceHome, 
-            ],
+            project_name: "Synap blog",
+            project_title: "Synap blog is a simple website blog using open API gorest",
             stacks: [
                 "Next JS",
                 "Tailwind",
+                "Typescript",
+                "SWR",
+                "Zustand",
+                "Yup"
             ],
-            links: [
-                {
-                    name: "Live website",
-                    link: "https://coding-nicely.vercel.app/",
-                    tag: <BiLinkExternal />
-                },
-                {
-                    name: "Source code",
-                    link: "https://github.com/TaufikAkbar7/Coding-nicely",
-                    tag: <FaGithub />
-                }
-            ]
+            linkWebsite: "https://synap-blog.vercel.app/",
+            linkGithub: "https://github.com/TaufikAkbar7/synap-blog"
         },
         {
-            id: 2,
-            project_name: "Shopmerce",
-            project_title: "Website E-commerce has features such as payment gateway, basket product, etc",
-            project_desc: "In March - Mei I was created website E-commerce using MERN (MongoDB, Express, React, Node) and for styling component iam using Framework Bootstrap. Shopmerce using REST API for communication between Back-end and Front-end, Shopmerce have two diffrent view: 1. View User, 2. View Admin in front-end view User iam using Redux and Redux Thunk for manage Shopmerce in single place and keep changes more predictable and traceable. Every E-commerce website must have payment gateway, Shopmerce have payment gateway using Midtrans. Why i choose Midtrans for payment gateway? because in Midtrans have a lot type of payment. In front-end view Admin iam using template Admin CoreUI and also using Redux, Redux Thunk too. For view Admin, focus on Create, Read, Update, Delete Product and User account.",
-            features: ["Dynamic Data", "Sign in and Sign Up", "CRUD Product", "Login with JWT", "Payment gateway", "Search Product", "Filtering  Product", "Basket Product", "CRUD Users"],
-            image: [
-                shopmerceHome, 
-                shopmerceImage1,
-                shopmerceImage2,
-                shopmerceImage3,
-                shopmerceImage4,
-                shopmerceImage5,
-            ],
+            project_name: "Cart Steel",
+            project_title: "Cart Steel is a simple E-commerce with feature homepage, add product to cart and checkout",
             stacks: [
-                "React JS",
-                "Bootstrap",
-                "Redux",
-                "Redux Thunk",
-                "CoreUI",
-                "Node JS",
-                "MongoDB",
-                "Express",
-                "JsonWebToken"
+                "Vue JS",
+                "Tailwind",
+                "Vite",
+                "Typescript",
+                "Pinia"
             ],
-            links: [
-                // {
-                //     name: "Code",
-                //     link: "https://coding-nicely.vercel.app/",
-                //     tag: <BiLinkExternal />
-                // },
-                {
-                    name: "Source code for Front-end",
-                    link: "https://github.com/TaufikAkbar7/Shopmerce-frontend",
-                    tag: <FaGithub />
-                },
-                {
-                    name: "Source code for Back-end",
-                    link: "https://github.com/TaufikAkbar7/Shopmerce-backend",
-                    tag: <FaGithub />
-                }
-            ]
+            linkWebsite: "https://cart-steel-sigma.vercel.app/",
+            linkGithub: "https://github.com/TaufikAkbar7/cart-steel"
         },
         {
-            id: 3,
+            project_name: "Coding Nicely",
+            project_title: "Coding Nicely is website landing page for startup based on course digital",
+            stacks: [
+                "Next JS",
+                "Tailwind",
+                "CSS",
+                "Javascript"
+            ],
+            linkWebsite: "https://coding-nicely.vercel.app/",
+            linkGithub: "https://github.com/TaufikAkbar7/Coding-nicely"
+        },
+        {
             project_name: "Job Apps",
             project_title: "First time project using React Native, just slicing design to code",
-            project_desc: "In August 2021 I was created a mobile app with name Job Apps. Job Apps is my first project in mobile app. I learn how to slicing design with mobile app using React Native.",
-            features: ["Search Job", "Archive Job", "Onboarding Screen"],
-            image: [
-                jobApps
-            ],
             stacks: [
                 "React Native",
+                "Javascript"
             ],
-            links: [
-                // {
-                //     name: "Code",
-                //     link: "https://coding-nicely.vercel.app/",
-                //     tag: <BiLinkExternal />
-                // },
-                {
-                    name: "Source code",
-                    link: "https://github.com/TaufikAkbar7/JobApps",
-                    tag: <FaGithub />
-                }
-            ]
+            linkGithub: 'https://github.com/TaufikAkbar7/JobApps'
         },
         {
-            id: 4,
             project_name: "Webfood App",
             project_title: "Simple Website for searching recipe food using Vue JS",
-            project_desc: "In November 2020 I was created a website recipe using Spoonacular API",
-            features: ["Search Recipe", "Get Recipe"],
-            image: [
-                webfood
-            ],
             stacks: [
-                "VueJS",
+                "Vue JS",
                 "Vuex",
                 "Vue-Bootstrap",
                 "SCSS"
             ],
-            links: [
-                {
-                    name: "Live Website",
-                    link: "https://webfood.vercel.app/",
-                    tag: <BiLinkExternal />
-                },
-                {
-                    name: "Source code",
-                    link: "https://github.com/TaufikAkbar7/JobApps",
-                    tag: <FaGithub />
-                }
-            ]
+            linkWebsite: "https://webfood.vercel.app/",
+            linkGithub: "https://github.com/TaufikAkbar7/JobApps",
         }
     ]
 }
@@ -172,15 +98,8 @@ const contact = {
             name: "Gmail",
             tag: <SiGmail />,
             link: "mailto:akbar.taufik062@gmail.com",
-        },
-        // {
-        //     name: "Resume",
-        //     tag: <FiDownload />,
-        //     link: "./TaufikAkbar-CV.pdf",
-        // },
+        }
     ],
-    no_telp: "+62 812-1326-0537",
-    address: "Jakarta Timur, DKI Jakarta / Indonesia"
 }
 
 export { hero, project, contact }
